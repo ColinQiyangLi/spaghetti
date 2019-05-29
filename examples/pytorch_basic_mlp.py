4006ac4467bc2b67d32df8322d98aee5315047ed
QiyangColinLi <colin.qiyang.li@gmail.com>
2019-05-29 00:18:56 -0400

    add mlp example

---
+++ examples/pytorch_basic_mlp.py
@@ -0,0 +1,24 @@
+import torch.nn as nn
+import spaghetti
+from spaghetti import register, quick_register, load, check
+
+quick_register(nn.Linear)
+quick_register(nn.ReLU)
+
+@quick_register
+class MLP(nn.Module):
+    def __init__(self, units, activation, linear_module):
+        super().__init__()
+        model = []
+        for index, (in_units, out_units) in enumerate(zip(units[:-1], units[1:])):
+            if index != 0:
+                model.append(activation)
+            model.append(linear_module(in_units, out_units))
+        self.model = nn.Sequential(*model)
+    
+    def forward(self, x):
+        return self.model(x)
+
+print(check())
+net = load("assets/pytorch_mlp.yaml")
+print(net)
